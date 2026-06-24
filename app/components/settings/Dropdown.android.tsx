fa6979ddc77abba73939c162418ef670111bc873
Keith Kurak <keith.kurak@gmail.com>
2026-06-24 01:43:37 -0400

    dropdown kinda works

--- app/components/settings/Dropdown.android.tsx
+++ app/components/settings/Dropdown.android.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import {
   ExposedDropdownMenuBox,
   ExposedDropdownMenu,
   DropdownMenuItem,
   TextField,
   Text,
+  useNativeState,
+  Row,
+  Icon,
+  Spacer,
 } from "@expo/ui/jetpack-compose"
-import { fillMaxWidth, menuAnchor } from "@expo/ui/jetpack-compose/modifiers"
+import { fillMaxWidth, menuAnchor, background, padding } from "@expo/ui/jetpack-compose/modifiers"
+import ArrowUpward from "@expo/material-symbols/arrow_drop_up.xml"
+import ArrowDownward from "@expo/material-symbols/arrow_drop_down.xml"
 
 type DropdownItem = {
   label: string
@@ -23,37 +29,58 @@ type DropdownProps = {
 export function Dropdown({ title, items, selectedValue, onValueChange }: DropdownProps) {
   const [expanded, setExpanded] = useState(false)
 
+  const selectedValueNativeState = useNativeState(
+    items.find((item) => item.value === selectedValue)?.label ?? "",
+  )
+
   const displayLabel = items.find((item) => item.value === selectedValue)?.label ?? "Select..."
 
+  useEffect(() => {
+    selectedValueNativeState.set(items.find((item) => item.value === selectedValue)?.label ?? "")
+  }, [selectedValue, selectedValueNativeState, items])
+
   return (
-    <ExposedDropdownMenuBox
-      expanded={expanded}
-      onExpandedChange={setExpanded}
+    <Row
+      verticalAlignment="center"
+      horizontalArrangement="spaceEvenly"
       modifiers={[fillMaxWidth()]}
     >
-      <TextField key={selectedValue} readOnly modifiers={[menuAnchor(), fillMaxWidth()]}>
-        <TextField.Label>
-          <Text>{title}</Text>
-        </TextField.Label>
-        <TextField.Placeholder>
-          <Text>{displayLabel}</Text>
-        </TextField.Placeholder>
-      </TextField>
-      <ExposedDropdownMenu expanded={expanded} onDismissRequest={() => setExpanded(false)}>
-        {items.map((item) => (
-          <DropdownMenuItem
-            key={item.value}
-            onClick={() => {
-              onValueChange(item.value)
-              setExpanded(false)
-            }}
-          >
-            <DropdownMenuItem.Text>
-              <Text>{item.label}</Text>
-            </DropdownMenuItem.Text>
-          </DropdownMenuItem>
-        ))}
-      </ExposedDropdownMenu>
-    </ExposedDropdownMenuBox>
+      <Text modifiers={[]}>State</Text>
+      <Spacer modifiers={[padding(0, 0, 16, 0)]} />
+      <ExposedDropdownMenuBox
+        expanded={expanded}
+        onExpandedChange={setExpanded}
+        modifiers={[fillMaxWidth()]}
+      >
+        <TextField
+          value={selectedValueNativeState}
+          key={selectedValue}
+          readOnly
+          modifiers={[menuAnchor(), fillMaxWidth(), background("#FFFFFF")]}
+        >
+          <TextField.Placeholder>
+            <Text>{displayLabel}</Text>
+          </TextField.Placeholder>
+          <TextField.TrailingIcon>
+            {expanded ? <Icon source={ArrowUpward} /> : <Icon source={ArrowDownward} />}
+          </TextField.TrailingIcon>
+        </TextField>
+        <ExposedDropdownMenu expanded={expanded} onDismissRequest={() => setExpanded(false)}>
+          {items.map((item) => (
+            <DropdownMenuItem
+              key={item.value}
+              onClick={() => {
+                onValueChange(item.value)
+                setExpanded(false)
+              }}
+            >
+              <DropdownMenuItem.Text>
+                <Text>{item.label}</Text>
+              </DropdownMenuItem.Text>
+            </DropdownMenuItem>
+          ))}
+        </ExposedDropdownMenu>
+      </ExposedDropdownMenuBox>
+    </Row>
   )
 }
