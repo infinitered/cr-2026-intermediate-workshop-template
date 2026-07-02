afda87a2ae3cdde1b66c9823aca7508f5d6faeb4
Frank Calise <fcalise@gmail.com>
2026-07-02 13:35:16 -0400

    chore(01-settings): clean up

--- app/components/DatePicker.android.tsx
+++ app/components/DatePicker.android.tsx
@@ -10,7 +10,6 @@ export function DatePicker({ title, value, onDateChange, maximumDate }: DatePick
     month: "short",
     day: "numeric",
     year: "numeric",
-    // TODO -write about this in the lesson native is in UTC and javascript is in local time, so we need to force the timezone to UTC to avoid off by one errors
     timeZone: "UTC",
   })
 
