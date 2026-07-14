75cab5fef2eb88b50f8ebb8bc8589d0bee9cac34
Keith Kurak <keith.kurak@gmail.com>
2026-07-14 14:52:54 -0400

    home layout

--- src/app/(tabs)/(home)/_layout.tsx
+++ src/app/(tabs)/(home)/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from "expo-router"
+import { Platform } from "react-native"
 
 export default function HomeStackLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Games", headerLargeTitle: true }}  />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Games",
+          headerLargeTitleEnabled: true,
+          ...(Platform.OS === "ios" && { headerTransparent: true }),
+          headerBlurEffect: "systemUltraThinMaterial",
+        }}
+      />
     </Stack>
   )
 }
